c15f8eb6bcc4ee6f3870126320266749caccf08f
codesver <codesver@gmail.com>
2024-04-23 17:45:56 +0900

    PostForm 만들기(타이핑 외우기)

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,9 +1,11 @@
+import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import styles from "./home.module.css";
 
 const HomePage = () => (
   <main className={styles.main}>
     <Tab />
+    <PostForm />
   </main>
 );
 
