841aff8caab19282b78d11042a48d2c5c5103f40
codesver <codesver@gmail.com>
2024-04-24 15:00:42 +0900

    usePathname 과 /explore 페이지

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,26 @@
-const ExplorePage = () => <main>검색 페이지</main>;
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
+import styles from "./explore.module.css";
+
+const ExplorePage = () => (
+  <main className={styles.main}>
+    <div className={styles.formZone}>
+      <SearchForm />
+    </div>
+    <div className={styles.trend}>
+      <h3>나를 위한 트렌드</h3>
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+      <Trend />
+    </div>
+  </main>
+);
 
 export default ExplorePage;
